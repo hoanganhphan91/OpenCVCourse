02c88f81d78a2466ad042b018b9987d25f0c5534
hoanganhphan91 <hoanganhphan91@gmail.com>
2023-06-03 09:59:18 +0700

    reading webcam

--- 02.ReadImg.py
+++ 02.ReadImg.py
@@ -1,5 +1,15 @@
 import cv2
 img = cv2.imread("Resource/Lenna.png")
+print(img.shape)
+
+imgResize = cv2.resize (img, (1000,1000))
+print(imgResize.shape)
+
+imgCropped = img [100:300, 100:300]
+
 
 cv2.imshow("Output",img)
+cv2.imshow("Resize",imgResize)
+cv2.imshow("Cropped",imgCropped)
+
 cv2.waitKey(0)
